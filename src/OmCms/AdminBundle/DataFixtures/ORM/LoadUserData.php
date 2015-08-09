fd01f8d6e96515e011e8d16d05946a157b0161f7
Vladimir Pak <TheRatW@gmail.com>
2015-08-09 06:41:02 +0300

    added admin role

--- src/OmCms/AdminBundle/DataFixtures/ORM/LoadUserData.php
+++ src/OmCms/AdminBundle/DataFixtures/ORM/LoadUserData.php
@@ -3,6 +3,7 @@ namespace OmCms\AdminBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use OmCms\AdminBundle\Entity\User\Group;
 use OmCms\AdminBundle\Entity\User\User;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -21,11 +22,16 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
      */
     public function load(ObjectManager $manager)
     {
+        $group = new Group();
+        $group->setName('admin');
+        $group->addRole('ROLE_SUPER_ADMIN');
+        $manager->persist($group);
+
         $user = new User();
         $user->setUsername('admin');
         $user->setEmail('admin@email.com');
         $user->setEnabled(true);
-        $user->setRoles(['ROLE_ADMIN']);
+        $user->addGroup($group);
 
         $encoder = $this->container
             ->get('security.encoder_factory')
@@ -33,6 +39,7 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
         $user->setPassword($encoder->encodePassword('admin', $user->getSalt()));
 
         $manager->persist($user);
+
         $manager->flush();
     }
 
