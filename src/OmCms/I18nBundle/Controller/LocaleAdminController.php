5a5d202438343a2030644b22fb2fcd1b6169d2f7
Vladimir Pak <TheRatW@gmail.com>
2015-08-09 19:45:40 +0300

    - tuning stof - first locale

---
+++ src/OmCms/I18nBundle/Controller/LocaleAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OmCms\I18nBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class LocaleAdminController extends CRUDController
+{
+
+}
---
+++ src/OmCms/I18nBundle/OmCmsI18nBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OmCms\I18nBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OmCmsI18nBundle extends Bundle
+{
+}
