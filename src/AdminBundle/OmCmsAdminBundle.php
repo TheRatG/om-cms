f64a214b3fae4e887c6a537059c77f536ee959a7
Vladimir Pak <TheRatW@gmail.com>
2015-08-20 23:01:53 +0300

    - real auto mapping for doctrine - tuning sonata news

--- src/AdminBundle/OmCmsAdminBundle.php
+++ src/AdminBundle/OmCmsAdminBundle.php
@@ -2,8 +2,13 @@
 
 namespace TheRat\OmCms\AdminBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OmCmsAdminBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
